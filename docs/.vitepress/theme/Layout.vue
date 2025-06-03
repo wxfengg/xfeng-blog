e9597c68268e257a20d3132234fa9321543c2168
XFeng <2214857812@qq.com>
2025-06-03 09:46:38 +0800

    feat: 深色模式过度动画

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,3 +1,60 @@
+<script lang="ts" setup>
+import Giscus from "@giscus/vue";
+import DefaultTheme from "vitepress/theme";
+import { watch, nextTick, provide } from "vue";
+import { inBrowser, useData } from "vitepress";
+
+const { isDark, page } = useData();
+
+const { Layout } = DefaultTheme;
+
+watch(isDark, (dark) => {
+  if (!inBrowser) return;
+
+  const iframe = document
+    .querySelector("giscus-widget")
+    ?.shadowRoot?.querySelector("iframe");
+
+  iframe?.contentWindow?.postMessage(
+    { giscus: { setConfig: { theme: dark ? "dark" : "light" } } },
+    "https://giscus.app"
+  );
+});
+
+const enableTransitions = () =>
+  'startViewTransition' in document &&
+  window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+
+provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y)
+    )}px at ${x}px ${y}px)`
+  ]
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
+
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 300,
+      easing: 'ease-in',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+    }
+  )
+})
+</script>
+
 <template>
   <Layout>
     <template #doc-footer-before> </template>
@@ -26,26 +83,29 @@
   </Layout>
 </template>
 
-<script lang="ts" setup>
-import Giscus from "@giscus/vue";
-import DefaultTheme from "vitepress/theme";
-import { watch } from "vue";
-import { inBrowser, useData } from "vitepress";
+<style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
 
-const { isDark, page } = useData();
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
 
-const { Layout } = DefaultTheme;
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
 
-watch(isDark, (dark) => {
-  if (!inBrowser) return;
+/* 切换主题样式 */
+/* .VPSwitchAppearance {
+  width: 22px !important;
+} */
 
-  const iframe = document
-    .querySelector("giscus-widget")
-    ?.shadowRoot?.querySelector("iframe");
-
-  iframe?.contentWindow?.postMessage(
-    { giscus: { setConfig: { theme: dark ? "dark" : "light" } } },
-    "https://giscus.app"
-  );
-});
-</script>
+/* .VPSwitchAppearance .check {
+  transform: none !important;
+} */
+</style>
