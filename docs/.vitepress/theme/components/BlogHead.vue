7ab7bf97ea4e778af4098d338ed7ed9d23ad655a
XFeng <wxfengg@gmail.com>
2025-06-04 23:30:16 +0800

    feat: 更新全局文章标题底栏组件

--- docs/.vitepress/theme/components/BlogHead.vue
+++ docs/.vitepress/theme/components/BlogHead.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-
+const props = defineProps({
+  author: {
+    type: String,
+    default: 'XFeng',
+  },
+  date: {
+    type: String,
+    default: new Date().toISOString().split('T')[0], // 默认当前日期
+  },
+  tags: {
+    type: String,
+    validator: (value: string) => ['文章📜', '碎碎念💬'].includes(value),
+    default: '文章📜',
+  },
+})
 </script>
 
 <template>
 <div class="blog-head">
-  ✏️XFeng &nbsp;&nbsp;&nbsp;丨&nbsp;&nbsp;&nbsp; 🕙2025-6-1 &nbsp;&nbsp;&nbsp;丨&nbsp;&nbsp;&nbsp; 碎碎念💬
+  ✏️ {{ props.author }} &nbsp;&nbsp;&nbsp;丨&nbsp;&nbsp;&nbsp; 🕙 {{ props.date }} &nbsp;&nbsp;&nbsp;丨&nbsp;&nbsp;&nbsp; {{ props.tags }}
 </div>
 </template>
 
